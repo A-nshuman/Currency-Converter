f484407f0b9e040b770decaed36673066cb7752d
Anshuman Bhardwaj <64587347+A-nshuman@users.noreply.github.com>
2023-11-29 21:14:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const amountInput = document.getElementById('amount');
     const convertButton = document.querySelector('button[type="submit"]');
     const convertedAmountDiv = document.getElementById('convertedAmount');
-    
+
     convertButton.addEventListener('click', () => {
         const fromCurrency = currencyFrom.value;
         const toCurrency = currencyTo.value;
         const amount = amountInput.value;
-    
+
         fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`)
             .then(response => response.json())
             .then(data => {
                 const conversionRate = data.rates[toCurrency];
                 const convertedAmount = amount * conversionRate;
-    
+
                 convertedAmountDiv.textContent = `${convertedAmount.toFixed(2)} ${toCurrency}`;
                 convertedAmountDiv.style.color = 'black';
             })
@@ -24,5 +24,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 console.log('An error occurred:', error);
                 convertedAmountDiv.textContent = 'Conversion failed. Please try again.';
             });
+    });
+
+    convertButton.addEventListener('mouseenter', () => {
+        convertButton.style.animation = 'buttonHoverAnim 0.5s ease-in-out forwards';
+    });
+
+    convertButton.addEventListener('click', () => {
+        convertButton.style.animation = 'buttonPressAnim 0.2s ease-in-out';
+        convertButton.addEventListener('animationend', () => {
+            convertButton.style.animation = 'none';
+        });
+    });
+
+    document.addEventListener('keyup', event => {
+        if (event.key === 'Enter') {
+            convertButton.click();
+        }
     });
 });
